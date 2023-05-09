f8ba530f34c9153e3453da4b224764bf34463b9f
mlodypies <simsd1234@gmail.com>
2023-05-09 20:05:38 +0200

    twig test 1

--- config.php
+++ config.php
@@ -1,4 +1,10 @@
 <?php
+require_once('vendor/autoload.php');
+
+$loader = new Twig\Loader\FilesystemLoader('templates');
+$twig = new Twig\Environment($loader);
+
+
 $db = new mysqli('localhost', 'root', '', 'loginform');
 
 ?>
---
+++ test.php
@@ -0,0 +1,8 @@
+<?php 
+
+
+require_once('config.php');
+
+$twig->display('test.html.twig');
+
+?>

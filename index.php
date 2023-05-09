bd37fd4f2db42fb33ceab3a21ad929f69d35325e
mlodypies <simsd1234@gmail.com>
2023-05-09 22:46:00 +0200

    routing rejestr login strona glowna

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <?php
+
+use Steampixel\Route;
+
 require_once('config.php');
 require_once('class/User.class.php');
 
-$user = new User('jkowalski', 'tajneHaslo');
-/*
-if($user->register()){
-    echo "zarejestrowano pomyslnie";
-} else {
-    echo "blad rejestracji uzytkownika"
-}
-*/
 
-if($user->login()) {
-    echo "zalogowano pomyslnie";
-} else {
-    echo "bledny login lub haslo";
-}
+Route::add('/', function() {
+    echo "Strona Glowna";
+});
 
+Route::add('/login', function() {
+    echo "Strona logowania";
+});
+
+Route::add('/register', function() {
+    echo "Strona rejestrowania";
+});
+
+Route::run('/loginform');
 ?>
